6ae0eef367810838ee84cabc51f2f6e0aaad3de8
Blake Spangenberg <blake.spangenberg1@gmail.com>
2022-01-19 13:54:54 -0800

    _config.yml

--- ci.sh
+++ ci.sh
@@ -12,5 +12,7 @@ echo "Installing Requirements"
 python -m pip install --upgrade -r requirements.txt
 
 echo "Building html pages"
+rm -rf build
 python -m randovania database export-videos --output-dir build
 cp README.md build
+cp _config.yml build
