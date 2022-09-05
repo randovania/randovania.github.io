c41cd78f21480c56d60730b7bdd91a4047d40d5d
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2022-09-05 14:27:52 +0200

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -3,11 +3,6 @@
 set -e -x
 cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
-echo "Setting up venv"
-python3.9 -m venv venv
-source venv/bin/activate
-python -c "import sys; assert sys.version_info[0:2] == (3, 9), 'Python 3.9 required'"
-
 echo "Installing Requirements"
 python -m pip install --upgrade -r requirements.txt
 
