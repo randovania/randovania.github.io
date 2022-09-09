af6cdc94fdd5a63c7137c0f19911c52a76673202
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2022-09-10 00:57:41 +0300

    Copy website from static_website and template the version

--- ci.sh
+++ ci.sh
@@ -3,11 +3,14 @@
 set -e -x
 cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
+randovania_version=4.5.0
+
 echo "Installing Requirements"
-python -m pip install --upgrade -r requirements.txt
+python -m pip install randovania==$randovania_version
 
 echo "Building html pages"
 rm -rf build
 python -m randovania database export-videos --output-dir build
-cp README.md build
+cp -ar static_website/* build/
+sed -i "s/{VERSION_TEMPLATE}/${randovania_version}/g" "build/install randovania.htm"
 cp _config.yml build
