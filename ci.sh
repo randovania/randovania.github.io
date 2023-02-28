0fb7796f8d186c6cbe90e0957d96e919571bdb16
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2023-02-28 20:46:32 +0100

    Update to 5.5.1

--- ci.sh
+++ ci.sh
@@ -3,7 +3,7 @@
 set -e -x
 cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
-randovania_version=5.4.0
+randovania_version=5.5.1
 
 echo "Installing Requirements"
 python -m pip install randovania==$randovania_version
