010ef8caa8438f9e9edfe36ea948b2523d5417d1
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2023-08-02 20:53:10 +0200

    6.1.0

--- ci.sh
+++ ci.sh
@@ -3,7 +3,7 @@
 set -e -x
 cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
-randovania_version=6.0.1
+randovania_version=6.1.0
 
 echo "Installing Requirements"
 python -m pip install randovania==$randovania_version cryptography
