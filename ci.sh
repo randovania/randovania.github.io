bf41ec96173092a5e23abed1d6dce077b6625603
Miepee <38186597+Miepee@users.noreply.github.com>
2023-12-26 22:18:05 +0100

    Update website to 7.1.1

--- ci.sh
+++ ci.sh
@@ -3,7 +3,7 @@
 set -e -x
 cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
-randovania_version=7.1.0
+randovania_version=7.1.1
 
 echo "Installing Requirements"
 python -m pip install randovania==$randovania_version cryptography
