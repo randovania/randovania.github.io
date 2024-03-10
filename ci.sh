cd390ce374ccca9d6709eab3f1909fd157389ceb
duncathan <dunc@duncathan.com>
2024-03-10 14:18:54 -0600

    evil CI changes to avoid refactoring video db for now

--- ci.sh
+++ ci.sh
@@ -8,9 +8,29 @@ randovania_version=7.4.0
 echo "Installing Requirements"
 python -m pip install randovania==$randovania_version cryptography
 
-echo "Building html pages"
+echo "Copying Jekyll source"
+echo "version: $randovania_version" >> _data/rdv.yml
 rm -rf build
-python -m randovania database export-videos --output-dir build
-cp -ar static_website/* build/
-sed -i "s/{VERSION_TEMPLATE}/${randovania_version}/g" "build/install randovania.htm"
-cp _config.yml build
+cp -ar ./ build/
+
+echo "Building video database pages"
+mkdir videos
+python -m randovania database export-videos --output-dir videos
+
+# insanely ugly hack!!! refactor ASAP!!!
+declare -A games
+games["Another Metroid 2 Remake"]=am2r
+games["Cave Story"]=cave_story
+games["Metroid Dread"]=dread
+games["Metroid Prime"]=prime1
+games["Metroid Prime 2 Echoes"]=prime2
+games["Metroid Prime 3 Corruption"]=prime3
+games["Metroid Samus Returns"]=samus_returns
+games["Super Metroid"]=super_metroid
+
+for i in "${!games[@]}"
+do
+    long="$i"
+    short="${games[$i]}"
+    cp "videos/$long" "build/games/$short/videos"
+done
