d4dcc229cf6fbc284763e24f45b51e9f090be928
Website-Version-Updater <invalid@example.com>
2024-12-06 02:02:27 +0000

    Update version to 8.7.1

--- ci.sh
+++ ci.sh
@@ -4,7 +4,7 @@ set -e -x
 shopt -s extglob
 cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
-randovania_version=8.7.0
+randovania_version=8.7.1
 echo "Installing Requirements"
 
 # python -m pip install randovania[website]==$randovania_version
