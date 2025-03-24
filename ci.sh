beda670f7f9484fd95608e62be93482116658ca1
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2025-03-24 22:55:33 +0200

    Split ci.sh, add dedicated file for version

--- ci.sh
+++ ci.sh
@@ -4,32 +4,7 @@ set -e -x
 shopt -s extglob
 cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 
-randovania_version=8.9.0
-echo "Installing Requirements"
-
-uv pip install randovania[website]==$randovania_version
-
-echo "Building video database pages"
-rm -rf _videos
-mkdir _videos
-uv run randovania development export-videos-yaml --output-dir _videos --as-frontmatter
-
-echo "Copying readme sections"
-rm -rf _includes/_readme
-mkdir _includes/_readme
-for section in WELCOME CREDITS
-do uv run randovania development export-readme-section --section $section --output-dir _includes/_readme
-done
-
-echo "Extracting game data"
-rm -rf assets/games
-rm -rf _games
-mkdir assets/games
-mkdir _games
-uv run randovania development extract-game-data --games-dir _games --covers-dir assets/games
-
-echo "Copying Jekyll source"
-echo "version: $randovania_version" > _data/rdv.yml
-rm -rf build
-mkdir build
-cp -ar !(build) build
+uv venv
+source .venv/bin/activate
+uv pip install randovania[website]==$(cat randovania-version.txt)
+./build.sh
