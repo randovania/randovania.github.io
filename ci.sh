26c24f5ea8b474a4254b07a1584e0015c1eb8905
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2025-03-24 22:38:34 +0200

    Use UV and remove hack

--- ci.sh
+++ ci.sh
@@ -7,22 +7,18 @@ cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 randovania_version=8.9.0
 echo "Installing Requirements"
 
-# python -m pip install randovania[website]==$randovania_version
-# FIXME: hack for missing README
-python -m pip download randovania==$randovania_version --no-deps --no-binary randovania
-tar xf randovania-$randovania_version.tar.gz
-python -m pip install -e ./randovania-$randovania_version[website]
+uv pip install randovania[website]==$randovania_version
 
 echo "Building video database pages"
 rm -rf _videos
 mkdir _videos
-python -m randovania development export-videos-yaml --output-dir _videos --as-frontmatter
+uv run randovania development export-videos-yaml --output-dir _videos --as-frontmatter
 
 echo "Copying readme sections"
 rm -rf _includes/_readme
 mkdir _includes/_readme
 for section in WELCOME CREDITS
-do python -m randovania development export-readme-section --section $section --output-dir _includes/_readme
+do uv run randovania development export-readme-section --section $section --output-dir _includes/_readme
 done
 
 echo "Extracting game data"
@@ -30,10 +26,7 @@ rm -rf assets/games
 rm -rf _games
 mkdir assets/games
 mkdir _games
-python -m randovania development extract-game-data --games-dir _games --covers-dir assets/games
-
-# Remove the extracted randovania so we don't accidentally deploy it
-rm -rf ./randovania-$randovania_version ./randovania-$randovania_version.tar.gz 
+uv run randovania development extract-game-data --games-dir _games --covers-dir assets/games
 
 echo "Copying Jekyll source"
 echo "version: $randovania_version" > _data/rdv.yml
